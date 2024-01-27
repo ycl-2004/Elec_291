bb54f8c95ba3f615f11bf5a38c4f98bd1c4400ae
ycl-2004 <141936790+ycl-2004@users.noreply.github.com>
2024-01-27 13:48:05 -0800

    Update ISR_example.asm

--- ISR_example.asm
+++ ISR_example.asm
@@ -285,7 +285,6 @@ time_hour_pressed:
 Timer2_ISR_da_sec:
 	da a 
 	mov time_sec_counter, a
-	;mov a, time_sec_counter
 	cjne a, #0x60, Timer2_ISR_done
 	clr a 
 	mov time_sec_counter, a
@@ -296,7 +295,6 @@ Timer2_ISR_da_sec:
 Timer2_ISR_da_min:
 	da a
 	mov time_min_counter, a 
-	;mov a, time_min_counter
 	cjne a, #0x60, Timer2_ISR_done
 	clr a 
 	mov time_min_counter, a
@@ -307,7 +305,6 @@ Timer2_ISR_da_min:
 Timer2_ISR_da_hour:
 	da a
 	mov time_hour_counter, a 
-	;mov a, time_hour_counter
 	cjne a, #0x12, Timer2_ISR_done
 	clr a 
 	mov time_hour_counter, a 
@@ -346,7 +343,6 @@ set_sec_pressed:
 	add a, #0x01
 	da a 
 	mov set_sec_counter, a 
-        ;mov a, set_sec_counter
 	cjne a, #0x60, Timer2_ISR_done
 	clr a 
 	mov set_sec_counter, a 
@@ -357,7 +353,6 @@ set_min_pressed:
 	add a, #0x01
 	da a 
 	mov set_min_counter, a 
-        ;mov a, set_min_counter
 	cjne a, #0x60, Timer2_ISR_done
 	clr a 
 	mov set_min_counter, a 
@@ -368,7 +363,6 @@ alarm_hour_set:
 	add a, #0x01
 	da a 
 	mov set_hour_counter, a 
-        ;mov a, set_hour_counter
 	cjne a, #0x12, Timer2_ISR_done
 	clr a
 	mov set_hour_counter, a 
